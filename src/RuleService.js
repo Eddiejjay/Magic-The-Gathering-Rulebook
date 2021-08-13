aac286ad0aafa840aaa02f3713e7a728b2aa75c6
Eero Mäki-Kokkila <eerojmk@gmail.com>
2021-08-13 10:50:41 +0300

    update

--- src/RuleService.js
+++ src/RuleService.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 // const baseUrl ='http:/localhost:3003/api/players'
 // Alemmalla toimii
 
-const heroku = 'https://cors-anywhere.herokuapp.com'
-const url = `${heroku}/https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt`
-
+// const heroku = 'https://cors-anywhere.herokuapp.com'
+// const url = `${heroku}/https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt`
+const origUrl ='https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt'
 
 export const getAll = async () => {
-  const response = await axios.get(url)
+  const response = await axios.get(origUrl)
   console.log('perse')
   console.log(typeof(response.data))
   // console.log(response.data)
