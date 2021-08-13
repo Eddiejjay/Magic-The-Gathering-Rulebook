fab7906b144e8acc331ed9b8f1c55cfa72e5d41e
Eero Mäki-Kokkila <eerojmk@gmail.com>
2021-08-13 12:23:25 +0300

    cors update

--- src/RuleService.js
+++ src/RuleService.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 // const baseUrl ='http:/localhost:3003/api/players'
 // Alemmalla toimii
 
-// const heroku = 'https://cors-anywhere.herokuapp.com'
-// const url = `${heroku}/https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt`
-const origUrl ='https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt'
+const heroku = 'https://young-island-54491.herokuapp.com'
+const url = `${heroku}/https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt`
+// const origUrl ='https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt'
 
 export const getAll = async () => {
-  const response = await axios.get(origUrl)
+  const response = await axios.get(url)
   console.log('perse')
   console.log(typeof(response.data))
   // console.log(response.data)
