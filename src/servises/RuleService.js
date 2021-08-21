dd46dd32a1c08c857c3f6eb49ac0ddea3c8a3ed7
Eero Mäki-Kokkila <eerojmk@gmail.com>
2021-08-22 02:54:04 +0300

    update

--- src/servises/RuleService.js
+++ src/servises/RuleService.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-undef */
 import axios from 'axios'
 
-const CORSPROXY = process.env.REACT_APP_CORSPROXY
-const RULESURL = process.env.REACT_APP_RULESURL
-const url = `${CORSPROXY}/${RULESURL}`
+// const CORSPROXY = process.env.REACT_APP_CORSPROXY
+// const RULESURL = process.env.REACT_APP_RULESURL
+// // const url = `${CORSPROXY}/${RULESURL}`
+const url2 = 'https://young-island-54491.herokuapp.com/https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt'
+
+
 
 export const getAll = async () => {
-  const response = await axios.get(url)
+  const response = await axios.get(url2)
+  console.log('url'. url )
   return response.data
 }
 
