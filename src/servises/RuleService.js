11e2d6fe45cee9fdeb29d1dbefdc386bb686a1b1
Eero Mäki-Kokkila <eerojmk@gmail.com>
2021-08-22 12:37:59 +0300

    data url fix

--- src/servises/RuleService.js
+++ src/servises/RuleService.js
@@ -1,15 +1,13 @@
 /* eslint-disable no-undef */
 import axios from 'axios'
 
-// const CORSPROXY = process.env.REACT_APP_CORSPROXY
-// const RULESURL = process.env.REACT_APP_RULESURL
-// // const url = `${CORSPROXY}/${RULESURL}`
-const url2 = 'https://young-island-54491.herokuapp.com/https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt'
-
+const CORSPROXY = process.env.REACT_APP_CORSPROXY
+const RULESURL = process.env.REACT_APP_RULESURL
+const url = `${CORSPROXY}/${RULESURL}`
 
 
 export const getAll = async () => {
-  const response = await axios.get(url2)
+  const response = await axios.get(url)
   console.log('url'. url )
   return response.data
 }
